32f53f393e170c88e637fa88fe4a41c272323058
Mariya Zinchenko <mariyazin2701@gmail.com>
2020-02-17 22:10:37 -0500

    wip: resolving authentication for firebase issue and addiding inventory page

--- Reliquary/src/app/app.module.ts
+++ Reliquary/src/app/app.module.ts
@@ -8,6 +8,28 @@ import { HttpClientModule } from '@angular/common/http';
 import { PokemonapiComponent } from './pokemonapi/pokemonapi.component';
 import { YugiohComponent } from './yugioh/yugioh.component';
 import { ImportDataComponent } from './import-data/import-data.component';
+import { InventoryComponent } from './inventory/inventory.component';
+import { HomeComponent } from './home/home.component';
+
+
+// 1. Import the libs you need
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+// 2. Add your credentials from step 1
+const config = {
+  apiKey: "0158da85b3c91e6ac92f72d3d4e70b9fab945b6e",
+  authDomain: "reliquary-22fc5.firebaseapp.com",
+  databaseURL: "https://reliquary-22fc5.firebaseio.com",
+  projectId: "reliquary-22fc5",
+  storageBucket: "reliquary-22fc5.appspot.com",
+  messagingSenderId: "sender-id",
+  appId: "app-id",
+  measurementId: "G-measurement-id",
+};
+
 
 @NgModule({
   declarations: [
@@ -15,12 +37,19 @@ import { ImportDataComponent } from './import-data/import-data.component';
     MtgIOComponent,
     PokemonapiComponent,
     YugiohComponent,
-    ImportDataComponent
+    ImportDataComponent,
+    InventoryComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Reliquary/src/app/import-data/import-data.component.ts
+++ Reliquary/src/app/import-data/import-data.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import * as admin from 'firebase-admin';
+
 
 @Component({
   selector: 'app-import-data',
@@ -8,8 +8,9 @@ import * as admin from 'firebase-admin';
 })
 export class ImportDataComponent implements OnInit {
 
+
   selectedFile: File = null;
-  database = admin.database;
+  // database = admin.database;
 
   constructor() {}
   onFileSelected(event) {
@@ -17,15 +18,14 @@ export class ImportDataComponent implements OnInit {
     this.selectedFile = event.target.files[0];
   }
   onUpload() {
-    if(this.selectedFile == null)
+    if (this.selectedFile == null) {
       return;
-    
-    const fd = new FormData();
-    fd.append('csv', this.selectedFile, this.selectedFile.name);
+    }
+    // const fd = new FormData();
+    // fd.append('csv', this.selectedFile, this.selectedFile.name);
 
-    debugger ;
 
-    //this.database.collection('Cards').doc('CardTable').set(fd);
+    // this.database.collection('Cards').doc('CardTable').set(fd);
   }
   ngOnInit() {
   }
