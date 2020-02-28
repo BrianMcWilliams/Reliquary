130f9eaac39d1e230023ff52a3e4acf5ef087bc3
Brian <brianmcwilliams15@gmail.com>
2020-02-27 22:18:18 -0500

    Authent

--- Reliquary/src/app/app.component.ts
+++ Reliquary/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { AuthService } from './services/auth.service';
+import * as admin from 'firebase-admin';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  constructor(public auth: AuthService) {}
+  constructor(public auth: AuthService) {
+    admin.initializeApp({
+      credential: admin.credential.applicationDefault(),
+      databaseURL: 'https://reliquary-22fc5.firebaseio.com'
+    });
+  }
 }
